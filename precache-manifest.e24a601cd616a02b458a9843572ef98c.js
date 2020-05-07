d957561d8d1ca4640b82ab7544ba149f4e56e0ca
Vladimir Bezobrazov <vovan0503@mail.ru>
2020-05-07 21:55:00 +0300

    Updates

---
+++ precache-manifest.e24a601cd616a02b458a9843572ef98c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d134987a87d82574a4f5e0d67a336ac",
+    "url": "/Kanban-board/index.html"
+  },
+  {
+    "revision": "87ef382b40bc93acfdcd",
+    "url": "/Kanban-board/static/css/main.109bf294.chunk.css"
+  },
+  {
+    "revision": "2a0a8362443172946c08",
+    "url": "/Kanban-board/static/js/2.cf7a7402.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Kanban-board/static/js/2.cf7a7402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ef382b40bc93acfdcd",
+    "url": "/Kanban-board/static/js/main.0f398fa8.chunk.js"
+  },
+  {
+    "revision": "17335882236e8ad3a56f",
+    "url": "/Kanban-board/static/js/runtime-main.4c68c124.js"
+  }
+]);
